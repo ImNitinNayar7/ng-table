cffb16132e3c5c5efd58a33582bf317efb334e75
Christian Crowhurst <ccrowhurst@ramplc.com>
2016-09-19 22:44:10 +0100

    fix demo-app code to work in firefox

--- demo-apps/es6-systemjs/src/my-table.component.js
+++ demo-apps/es6-systemjs/src/my-table.component.js
@@ -1,31 +1,34 @@
 import 'ng-table';
 
-class MyTableController {
-    constructor(NgTableParams) {
-        var data = [
-            { name: "Moroni", age: 50 },
-            { name: "Tiancum", age: 43 },
-            { name: "Jacob", age: 27 },
-            { name: "Nephi", age: 29 },
-            { name: "Enos", age: 34 },
-            { name: "Tiancum", age: 43 },
-            { name: "Jacob", age: 27 },
-            { name: "Nephi", age: 29 },
-            { name: "Enos", age: 34 },
-            { name: "Tiancum", age: 43 },
-            { name: "Jacob", age: 27 },
-            { name: "Nephi", age: 29 },
-            { name: "Enos", age: 34 },
-            { name: "Tiancum", age: 43 },
-            { name: "Jacob", age: 27 },
-            { name: "Nephi", age: 29 },
-            { name: "Enos", age: 34 }
-        ];
+// note: because of Firefox (v45-v48) native class syntax causes a problem in angular.
+// This is not likely going to be fixed in angular as it's a bug in the browser deviating from the spec
+// Therefore if you do not want a transpiler step, you will NOT be able to use class syntax
+// For more details see: https://github.com/angular/angular.js/issues/14240#issuecomment-197418167
 
-        this.tableParams = new NgTableParams({}, {
-            dataset: data
-        });
-    }
+function MyTableController(NgTableParams) {
+    var data = [
+        { name: "Moroni", age: 50 },
+        { name: "Tiancum", age: 43 },
+        { name: "Jacob", age: 27 },
+        { name: "Nephi", age: 29 },
+        { name: "Enos", age: 34 },
+        { name: "Tiancum", age: 43 },
+        { name: "Jacob", age: 27 },
+        { name: "Nephi", age: 29 },
+        { name: "Enos", age: 34 },
+        { name: "Tiancum", age: 43 },
+        { name: "Jacob", age: 27 },
+        { name: "Nephi", age: 29 },
+        { name: "Enos", age: 34 },
+        { name: "Tiancum", age: 43 },
+        { name: "Jacob", age: 27 },
+        { name: "Nephi", age: 29 },
+        { name: "Enos", age: 34 }
+    ];
+
+    this.tableParams = new NgTableParams({}, {
+        dataset: data
+    });
 }
 
 MyTableController.$inject = ['NgTableParams'];
